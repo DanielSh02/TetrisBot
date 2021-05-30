df6072eadcae8a9689fcf5c02dcb1329ed987856
wasp-soup <cheung.marcus@gmail.com>
2021-05-30 15:16:17 +0800

    d

---
+++ trainer.py
@@ -0,0 +1,9 @@
+class Generation():
+    def __init__(ancestor=None):
+        self.ancestor=ancestor
+
+#Bumpiness of the board (more bumpy probably worse?? might depend on the piece) negative weight
+
+#Number of holes created (more is worse) negative weight
+
+#Difference in height (greater is worse) negative weight (might be similar to height)
