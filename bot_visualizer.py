834425ef7aea637c097b800c7a01e5ec5135249a
wasp-soup <cheung.marcus@gmail.com>
2021-06-10 02:09:54 +0800

    Update bot_visualizer.py

--- bot_visualizer.py
+++ bot_visualizer.py
@@ -5,7 +5,7 @@ from pyglet import shapes
 from pyglet.window import key
 
 bot = Competitor('epicstyle')
-bot.weights = [0.3023643717256892, 0.04609843452110984, 0.0971269257666385]
+bot.weights = [0.4397317180301542, 0.28839572890950493, 0.00925338110727092]
 # Holes: 0.3023643717256892, Height difference: 0.04609843452110984, Line clearing: 0.0971269257666385
 gamestate = bot.gamestate
 
