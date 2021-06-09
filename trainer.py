147358d58ef072d4b42dabff2b5abd31ea687513
wasp-soup <cheung.marcus@gmail.com>
2021-06-09 16:48:46 +0800

    Data Saving

--- trainer.py
+++ trainer.py
@@ -3,8 +3,9 @@ import pickle
 import glob
 
 def generate_bot(desired_score):
-    species_num = len(glob.glob("species/*"))
+    species_num = len(glob.glob(f"species/{desired_score}_v*"))
     current_gen = Generation()
+    file = open(f"species/{desired_score}_v{species_num}.pickle", "wb")
     #while score is still less than desired score
     while max([comp.gamestate.score for comp in current_gen.competitors]) < desired_score:
         #train the generation
@@ -12,10 +13,9 @@ def generate_bot(desired_score):
         #update current gen to be the kids
         current_gen = Generation(current_gen)
     print('Success')
-    file = open(f"species/species_{species_num}.pickle", "wb")
+    
     pickle.dump(current_gen, file)
     file.close()
 
-
 for i in range(10):
-    generate_bot(10000)
+    generate_bot(3000)
